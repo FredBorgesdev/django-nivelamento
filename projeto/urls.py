eec9ea0f16b8f5fe28713c29374f39828b78d253
Fredson Filho <fredborgesfilho@gmail.com>
2025-12-29 10:11:33 -0300

    create templates

--- projeto/urls.py
+++ projeto/urls.py
@@ -17,8 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-
+# dominio/recipes
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('recipes.urls'))
+    path('', include('recipes.urls')),
+    path('recipes/', include('recipes.urls')),
 ]
--- recipes/urls.py
+++ recipes/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
+
 from recipes.views import home, contato, sobre
 
 
 urlpatterns = [
     path('', home),
+    path('sobre/', sobre),
     path('contato/', contato),
-    path('sobre/', sobre)
 ]

59781f35da0abe36a7a099300eb8c7986bfde258
Fredson Filho <fredborgesfilho@gmail.com>
2026-03-05 11:05:23 -0300

    Temp example

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/home.html', context = {
+        'name': 'Fredson'})
 
 
 def contato(request):
-    return HttpResponse('CONTATO')
+    return render(request, 'me-apague/temp.html')
 
 
 def sobre(request):
